2d5311e30ca300918227e6ad20ee73b9dc0af249
Lucas GUILLEROT <lguiller@e1r5p13.42.fr>
2018-09-12 13:51:50 +0200

    ++

--- srcs/input_device.c
+++ srcs/input_device.c
@@ -6,7 +6,7 @@
 /*   By: bede-fre <bede-fre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/26 13:24:19 by bede-fre          #+#    #+#             */
-/*   Updated: 2018/07/25 11:45:13 by lguiller         ###   ########.fr       */
+/*   Updated: 2018/09/06 13:14:32 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int		ft_button_press(int key, int x, int y, t_all *all)
 		all->p.ray_infos = 1;
 		all->a = a;
 		ft_print_ray_infos(all);
-		system("leaks wolf3d");
+		system("leaks wolf3d | grep 'leaked bytes' | cut -c16- | cut -c-7");
 		all->p.ray_infos = 0;
 	}
 	return (0);
